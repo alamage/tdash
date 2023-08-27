634f9ccdddc1ae4c3a7d7754b69aa1f0f339c575
mageic-dev <malam@mageic.com>
2023-08-27 00:08:09 -0400

    rust formatting

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri_plugin_log::{LogTarget};
 use log::{info, warn};
-use system_shutdown::{sleep,reboot,shutdown};
 use std::process::Command;
+use system_shutdown::{reboot, shutdown, sleep};
+use tauri_plugin_log::LogTarget;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -49,14 +49,14 @@ fn get_volume() -> i32 {
     let vol = &vol_percent[0..vol_percent.len() - 1];
     let vol_i32: i32 = vol.parse().unwrap();
     //println!("{}", vol);
-    return vol_i32;
+    vol_i32
 }
 
 #[tauri::command]
 fn set_volume(volume: i32) {
-    let vol_str = format!("{}%", volume.to_string());
+    let vol_str = format!("{}%", volume);
 
-    let output = Command::new("pactl")
+    let _output = Command::new("pactl")
         .arg("set-sink-volume")
         .arg("0")
         .arg(vol_str)
@@ -76,12 +76,12 @@ fn get_mute_state() -> bool {
     let words = output_stdout.split_whitespace().collect::<Vec<&str>>();
     let state = words.get(1).unwrap();
 
-    return state == &"yes";
+    state == &"yes"
 }
 
 #[tauri::command]
 fn toggle_mute() {
-    let output = Command::new("pactl")
+    let _output = Command::new("pactl")
         .arg("set-sink-mute")
         .arg("0")
         .arg("toggle")
@@ -91,19 +91,21 @@ fn toggle_mute() {
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_log::Builder::default().targets([
-            LogTarget::LogDir,
-            LogTarget::Stdout,
-            LogTarget::Webview,
-        ]).build())
-        .invoke_handler(tauri::generate_handler![greet, 
-                        system_sleep, 
-                        system_reboot, 
-                        system_shutdown, 
-                        get_volume, 
-                        set_volume,
-                        get_mute_state, 
-                        toggle_mute])
+        .plugin(
+            tauri_plugin_log::Builder::default()
+                .targets([LogTarget::LogDir, LogTarget::Stdout, LogTarget::Webview])
+                .build(),
+        )
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            system_sleep,
+            system_reboot,
+            system_shutdown,
+            get_volume,
+            set_volume,
+            get_mute_state,
+            toggle_mute
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
